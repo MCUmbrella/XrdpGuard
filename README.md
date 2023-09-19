7dd87d8013631f86f0b0ea31350916749e68142e
MCUmbrella <mcumbrella@protonmail.com>
2023-09-19 21:53:08 +0800

    improve the way of detecting login result

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-```shell
-XrdpGuard [--log=日志路径] [--period=时间跨度] [--maxFail=最大失败次数] [--impl=防火墙管理类路径] [--debug] [--dryrun]
+```
+Usage: java -jar XrdpGuard.jar [options]
+Options:
+    --help          Show the help message and exit.
+    --log={}        Specify the path of XRDP log. (Default: /var/log/xrdp.log)
+    --period={}     Specify the time period (in milliseconds) to consider for
+                    login failures. (Default: 10 minutes)
+    --maxFail={}    Specify the maximum number of login failures allowed before
+                    an IP address is considered suspicious. (Default: 3)
+    --impl={}       Specify the firewall manager implementation class to use.
+                    (Default: vip.floatationdevice.xrdpguard.firewall.Firewalld)
+    --debug         Enable debug output.
+    --dryrun        Perform a dry run: only show suspicious IP(s) and do not
+                    modify the firewall.
+    --export        Print the extracted login records to stdout and exit.
 ```
 Java 8 is required
--- src/vip/floatationdevice/xrdpguard/XrdpGuard.java
+++ src/vip/floatationdevice/xrdpguard/XrdpGuard.java
@@ -16,6 +16,21 @@ import java.util.logging.Logger;
 public class XrdpGuard
 {
     private static final String VERSION = "0.1.0";
+    private static final String HELP_MESSAGE = "XrdpGuard version " + VERSION +
+            "\nUsage: java -jar XrdpGuard.jar [options]\n" +
+            "Options:\n" +
+            "    --help          Show the help message and exit.\n" +
+            "    --log={}        Specify the path of XRDP log. (Default: /var/log/xrdp.log)\n" +
+            "    --period={}     Specify the time period (in milliseconds) to consider for\n" +
+            "                    login failures. (Default: 10 minutes)\n" +
+            "    --maxFail={}    Specify the maximum number of login failures allowed before\n" +
+            "                    an IP address is considered suspicious. (Default: 3)\n" +
+            "    --impl={}       Specify the firewall manager implementation class to use.\n" +
+            "                    (Default: vip.floatationdevice.xrdpguard.firewall.Firewalld)\n" +
+            "    --debug         Enable debug output.\n" +
+            "    --dryrun        Perform a dry run: only show suspicious IP(s) and do not\n" +
+            "                    modify the firewall.\n" +
+            "    --export        Print the extracted login records to stdout and exit.";
     private static final Logger l = Logger.getLogger("XrdpGuard");
     private static final ArrayList<String> filteredLoginTimeAddr = new ArrayList<>();
     private static final ArrayList<Boolean> filteredLoginResult = new ArrayList<>();
@@ -33,29 +48,29 @@ public class XrdpGuard
     public static void main(String[] args) throws Exception
     {
         // 解析命令行参数（如果存在）
-        // 带参数用法：XrdpGuard [--log=日志路径] [--period=时间跨度] [--maxFail=最大失败次数] [--impl=防火墙管理类路径] [--debug] [--dryrun]
         for(String a : args)
         {
-            if(a.equals("--help"))
+            if(a.equals("--help")) // 打印帮助信息后退出
             {
-                System.out.println("XrdpGuard [--log={}] [--period={}] [--maxFail={}] [--impl={}] [--debug] [--dryrun] [--export]");
+                System.out.println(HELP_MESSAGE);
                 System.exit(0);
             }
-            else if(a.startsWith("--log="))
+            else if(a.startsWith("--log=")) // 指定XRDP日志的路径
                 logPath = a.substring(6);
-            else if(a.startsWith("--period="))
+            else if(a.startsWith("--period=")) // 设置时间范围（毫秒）
                 periodMs = Long.parseLong(a.substring(9));
-            else if(a.startsWith("--maxFail="))
+            else if(a.startsWith("--maxFail=")) // 设置时间范围内允许的最大登录失败次数
                 maxFails = Integer.parseInt(a.substring(10));
-            else if(a.startsWith("--impl="))
+            else if(a.startsWith("--impl=")) // 设置要使用的防火墙管理器类的路径
                 fwClassPath = a.substring(7);
-            else if(a.equals("--debug"))
+            else if(a.equals("--debug")) // 开启调试输出
                 flDebug = true;
-            else if(a.startsWith("--dryrun"))
+            else if(a.startsWith("--dryrun")) // 开启演练模式
                 flDryRun = true;
-            else if(a.startsWith("--export"))
+            else if(a.startsWith("--export")) // 开启导出模式
                 flExportMode = true;
         }
+
         // 准备工作
         ConsoleHandler consoleHandler = new ConsoleHandler();
         consoleHandler.setFormatter(new LoggerFormatter());
@@ -70,37 +85,58 @@ public class XrdpGuard
         }
         l.config("Configurations:\n\tLog file: " + logPath + "\n\tTime period (ms): " + periodMs + "\n\tMax fail count: " + maxFails + "\n\tFirewall manager: " + fwClassPath);
         fw = (FirewallManager) Class.forName(fwClassPath).newInstance();
+
+        // 开始逐行读取日志
         l.fine("Reading " + logPath);
         BufferedReader br = new BufferedReader(new FileReader(logPath), 32768);
-        String line;
         StringBuilder sb = new StringBuilder();
-        // 开始逐行读取日志
-        while((line = br.readLine()) != null)
+        // xrdp.log中的客户端连入记录和登录成功记录不在同一行输出，所以需要至少两行才能判定登录是否成功
+        String line;
+        String lastIncomingConnectionLine = null;
+        while(true)
         {
+            line = br.readLine();
+            if(line == null) // 检查是否读取到了文件末尾
+            {
+                // 如果到达了文件末尾，而且上条连入记录没有匹配到登录成功记录，将其标记为登录失败
+                if(lastIncomingConnectionLine != null)
+                {
+                    filteredLoginTimeAddr.add(lastIncomingConnectionLine);
+                    filteredLoginResult.add(true);
+                }
+                break;
+            }
             // 首先检测日志开始标志。程序将从最后遇到的日志开始标志处开始扫描
             if(line.contains("starting xrdp"))
             {
                 // 检测到新的日志开头，前面的记录作废
                 filteredLoginTimeAddr.clear();
                 filteredLoginResult.clear();
+                lastIncomingConnectionLine = null;
             }
-            // 按日志条目类型把客户端连入记录和登录结果记录分别存储，因为两种记录不在同一行输出
-            if(line.contains("connection received from")) // 客户端连入标志
+            // 检查客户端连入标志
+            else if(line.contains("connection received from"))
             {
+                if(lastIncomingConnectionLine != null)
+                {
+                    // 上条连入记录没有匹配到登录成功记录，将其标记为登录失败
+                    filteredLoginTimeAddr.add(lastIncomingConnectionLine);
+                    filteredLoginResult.add(true);
+                }
+                // 更新上条连入记录为此次记录
                 sb.setLength(0);
                 sb.append(line, 1, 18).append(' ').append(line, line.indexOf("from ") + 5, line.indexOf(" port"));
-                filteredLoginTimeAddr.add(sb.toString());
+                lastIncomingConnectionLine = sb.toString();
             }
-            else if(line.contains("] login fail") ||
-                    line.contains("SSL_accept: I/O error") ||
-                    line.contains("libxrdp_force_read: header read error")
-            ) // 客户端登录失败标志
+            // 检查客户端登录成功标志
+            else if(line.contains("] login succ"))
             {
-                filteredLoginResult.add(true);
-            }
-            else if(line.contains("] login succ")) // 客户端登录成功标志
-            {
+                if(lastIncomingConnectionLine == null)
+                    continue;
+                // 存在上条连入记录且匹配到了登录成功记录，将其标记为登录成功
+                filteredLoginTimeAddr.add(lastIncomingConnectionLine);
                 filteredLoginResult.add(false);
+                lastIncomingConnectionLine = null;
             }
         }
         // 检查客户端连入次数和登录结果显示次数是否相同。如果不同，程序会报错并退出来避免错位导致的误判
@@ -137,7 +173,7 @@ public class XrdpGuard
         }
 
         // 创建一个Map来统计每个IP地址的失败登录次数
-        HashMap<String, Integer> ipFailCounts = new HashMap<>();
+        HashMap<String, Integer> ipFailCounter = new HashMap<>();
         // 获取当前时间
         Date now = new Date();
         // 遍历登录记录
@@ -147,18 +183,19 @@ public class XrdpGuard
             long timeDiff = now.getTime() - login.time.getTime();
             // 如果时间差在时间跨度内且登录失败，更新IP地址的失败计数
             if(timeDiff <= periodMs && login.fail)
-                ipFailCounts.put(login.addr, ipFailCounts.getOrDefault(login.addr, 0) + 1);
+                ipFailCounter.put(login.addr, ipFailCounter.getOrDefault(login.addr, 0) + 1);
         }
         logins.clear();
         // 找到登录失败次数达到指定次数或更多的IP地址
         ArrayList<String> suspiciousIPs = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry : ipFailCounts.entrySet())
+        for(Map.Entry<String, Integer> entry : ipFailCounter.entrySet())
             if(entry.getValue() >= maxFails)
                 suspiciousIPs.add(entry.getKey());
-        ipFailCounts.clear();
+        ipFailCounter.clear();
         // 输出符合条件的IP地址列表
         l.info("Suspicious IPs (" + suspiciousIPs.size() + "): " + suspiciousIPs);
 
+        // 演练模式：只输出将要被封禁的IP，不执行实际操作
         if(flDryRun)
         {
             l.info("Dry run completed");
