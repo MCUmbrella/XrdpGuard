be6a3a29abbb745083900b99a4d8174fa128b6a1
MCUmbrella <mcumbrella@protonmail.com>
2023-10-10 11:22:48 +0800

    small changes

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Usage: java -jar XrdpGuard.jar [options]
 Options:
     --help          Show the help message and exit.
     --log={}        Specify the path of XRDP log. (Default: /var/log/xrdp.log)
+    --banlog={}     Specify the path of the file to save ban records to.
+                    (Default: xrdpguard/ban.log)
+    --whitelist={}  Specify the path of the file to load IP whitelist.
+                    (Default: xrdpguard/whitelist.txt)
     --period={}     Specify the time period (in milliseconds) to consider for
                     login failures. (Default: 10 minutes)
     --maxfail={}    Specify the maximum number of login failures allowed before
@@ -16,6 +20,6 @@ Options:
     --dryrun        Perform a dry run: only show suspicious IP(s) and do not
                     modify the firewall.
     --export        Print the extracted login records to stdout and exit.
-    --nobanlog      Do not save ban records to "xrdpguard/ban.log".
+    --nobanlog      Do not save ban records. Overrides "--banlog={}"
 ```
 Java 8 is required
--- src/vip/floatationdevice/xrdpguard/XrdpGuardCommons.java
+++ src/vip/floatationdevice/xrdpguard/XrdpGuardCommons.java
@@ -13,9 +13,9 @@ public class XrdpGuardCommons
             "Options:\n" +
             "    --help          Show the help message and exit.\n" +
             "    --log={}        Specify the path of XRDP log. (Default: /var/log/xrdp.log)\n" +
-            "    --banlog={}     Specify the path to save the ban log file.\n" +
+            "    --banlog={}     Specify the path of the file to save ban records to.\n" +
             "                    (Default: xrdpguard/ban.log)\n" +
-            "    --whitelist={}  Specify the path to load the IP whitelist file.\n" +
+            "    --whitelist={}  Specify the path of the file to load IP whitelist.\n" +
             "                    (Default: xrdpguard/whitelist.txt)\n" +
             "    --period={}     Specify the time period (in milliseconds) to consider for\n" +
             "                    login failures. (Default: 10 minutes)\n" +
@@ -30,7 +30,7 @@ public class XrdpGuardCommons
             "    --dryrun        Perform a dry run: only show suspicious IP(s) and do not\n" +
             "                    modify the firewall.\n" +
             "    --export        Print the extracted login records to stdout and exit.\n" +
-            "    --nobanlog      Do not save ban records to \"xrdpguard/ban.log\".";
+            "    --nobanlog      Do not save ban records. Overrides \"--banlog={}\"";
     private static final SimpleDateFormat XRDP_TIME_FMT = new SimpleDateFormat("yyyyMMdd-HH:mm:ss");
     private static final SimpleDateFormat XG_TIME_FMT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
     private static final Pattern IPV4_PATTERN = Pattern.compile("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
